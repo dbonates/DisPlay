15c06a2b8179d9d710614f11aff1a11f8a626997
Daniel Bonates <dbonates@gmail.com>
2021-11-25 12:28:49 -0300

    first version - bare bones

--- DisPlay/ViewController.swift
+++ DisPlay/ViewController.swift
@@ -8,12 +8,134 @@
 import UIKit
 
 class ViewController: UIViewController {
-
-    override func viewDidLoad() {
-        super.viewDidLoad()
-        // Do any additional setup after loading the view.
+    
+    @IBOutlet weak var takeLbl: UILabel!
+    
+    var fonts = ["TrimPosterWebCondensed", "Bebas Neue"]
+    var currentFont = 0
+    
+    var takeNumber: Int {
+        return UserDefaults.standard.integer(forKey: "currentTakeNumber")
     }
 
+    var darkMode = true
+    
+    override func viewDidLoad() {
+        
+        super.viewDidLoad()
+        setupFonts()
+        setupDisplay()
+        setupGestures()
+        navigationController?.navigationBar.barStyle = .default
 
+        NotificationCenter.default.addObserver(
+                    self,
+                    selector: #selector(orientationChanged),
+                    name: UIDevice.orientationDidChangeNotification,
+                    object: nil
+                );
+    }
+    
+    func setupFonts() {
+        //TrimPosterWebCondensed
+//        for family in UIFont.familyNames.sorted() {
+//            let names = UIFont.fontNames(forFamilyName: family)
+//            print("Family: \(family) Font names: \(names)")
+//        }
+    }
+    
+    func setupDisplay(_ changingTakeOnly: Bool = false) {
+        
+        let duration = changingTakeOnly ? 0 : 0.25
+        UIView.transition(with: takeLbl, duration: duration,
+                          options: .transitionCrossDissolve,
+                          animations: {
+            if self.darkMode {
+                self.takeLbl.textColor = .cyan
+                self.view.backgroundColor = .black
+                self.takeLbl.layer.shadowColor = UIColor.cyan.cgColor
+                
+            } else {
+                self.takeLbl.textColor = .black
+                self.view.backgroundColor = .white
+                self.takeLbl.layer.shadowColor = UIColor.lightGray.cgColor
+            }
+        }, completion: nil)
+        
+        
+        
+        takeLbl.layer.shadowRadius = 4.0
+        takeLbl.layer.shadowOpacity = 0.9
+        takeLbl.layer.shadowOffset = .zero
+        takeLbl.layer.masksToBounds = false
+        setNeedsStatusBarAppearanceUpdate()
+        guard let customFont = UIFont(name: fonts[currentFont], size: UIFont.labelFontSize) else {
+            fatalError("""
+                Failed to load font.
+                Make sure the font file is included in the project and the font name is spelled correctly.
+                """
+            )
+        }
+
+        takeLbl.font = UIFontMetrics.default.scaledFont(for: customFont)
+        takeLbl.adjustsFontSizeToFitWidth = true
+        takeLbl.text = "\(takeNumber)"
+        takeLbl.font = takeLbl.font.withSize(takeLbl.frame.height * 2.5/3)
+        takeLbl.isUserInteractionEnabled = false
+    }
+    
+    func setupGestures() {
+        let tappedGest = UITapGestureRecognizer(target: self, action: #selector(addTake))
+        view.addGestureRecognizer(tappedGest)
+
+        let swipeDownGest = UISwipeGestureRecognizer(target: self, action: #selector(switchFont))
+        swipeDownGest.direction = .down
+        view.addGestureRecognizer(swipeDownGest)
+        
+        let swipeTopGest = UISwipeGestureRecognizer(target: self, action: #selector(switchTheme))
+        swipeTopGest.direction = .up
+        view.addGestureRecognizer(swipeTopGest)
+        
+        let longPressGest = UILongPressGestureRecognizer(target: self, action: #selector(reset))
+        longPressGest.minimumPressDuration = 1.5
+        view.addGestureRecognizer(longPressGest)
+    }
+    
+    @objc func switchFont(_ swipeDownGest: UISwipeGestureRecognizer) {
+        currentFont += 1
+        if currentFont >= fonts.count {
+            currentFont = 0
+        }
+        setupDisplay()
+    }
+    
+    @objc func switchTheme(_ swipeTopGest: UISwipeGestureRecognizer) {
+        darkMode = !darkMode
+        setupDisplay()
+    }
+    
+    @objc func reset(_ longPressGest: UILongPressGestureRecognizer) {
+        UserDefaults.standard.set(0, forKey: "currentTakeNumber")
+        setupDisplay()
+    }
+    
+    @objc func addTake(_ tapGest: UITapGestureRecognizer) {
+        let point = tapGest.location(in: view)
+        let increment = point.x > view.bounds.width/2 ? 1 : -1
+        UserDefaults.standard.set(takeNumber + increment, forKey: "currentTakeNumber")
+        setupDisplay(true)
+    }
+
+    @objc func orientationChanged(_ notification: NSNotification) {
+        setupDisplay()
+    }
+    
+    override var preferredStatusBarStyle: UIStatusBarStyle {
+        if darkMode {
+            return .lightContent
+        } else {
+            return .darkContent
+        }
+    }
 }
 
